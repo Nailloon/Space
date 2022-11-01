88c5a633fc55ae2b0638552e98b648d42d24988e
Artem_Filyakin <artemfilyakin5@gmail.com>
2022-11-02 02:14:05 +0600

    Added classes Vector and MoveCommand. Added ICommand and IMovable. Tests are sucessful

---
+++ SpaceBattle.Lib.Test/VectorTest.cs
@@ -0,0 +1,29 @@
+using Moq;
+using SpaceBattle.Classes;
+namespace SpaceBattle.Lib.Test
+{
+    public class VectorTest
+    {
+        [Fact]
+        public void TestSameSize()
+        {
+            var first = new Vector(8, 10, 1, 3, 7);
+            var second = new Vector(9, 3, 2, 2, 4);
+            var third = new Vector(11, 22, 33);
+            Assert.True(Vector.AreSameSize(first, second));
+            Assert.False(Vector.AreSameSize(second, third));
+        }
+
+        [Fact]
+        public void TestSum()
+        {
+            var first = new Vector(1, 2, 3, 5);
+            var second = new Vector(2, 4, 6, 10);
+            var third = new Vector(3, 6, 9, 15);
+            var fourth = new Vector(11, 22, 33);
+            Assert.True(Vector.AreEquals(third, first + second));
+            Assert.True(Vector.AreEquals(third, Vector.Summa(second, first)));
+            Assert.Throws<ArgumentException>(() => Vector.Summa(first, fourth));
+        }
+    }
+}
