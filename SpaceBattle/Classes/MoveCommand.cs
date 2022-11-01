3d936c1dee2b4b4cd1c2b78c03e17683ceea8df5
Artem_Filyakin <artemfilyakin5@gmail.com>
2022-11-02 02:14:05 +0600

    Added classes Vector and MoveCommand. Added ICommand and IMovable. Tests are sucessful

---
+++ SpaceBattle/Classes/MoveCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpaceBattle.Interfaces;
+
+namespace SpaceBattle.Classes
+{
+    public class MoveCommand : ICommand
+    {
+        private IMovable movable;
+        public MoveCommand(IMovable movable)
+        {
+            this.movable = movable;
+        }
+
+        public void Execute()
+        {
+            movable.Position += movable.Velocity;
+        }
+    }
+}
