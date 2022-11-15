4518b5be50f82a031f99b7611b6a79ec0094b7d5
Artem_Filyakin <artemfilyakin5@gmail.com>
2022-11-16 00:01:06 +0600

    Remade tests

--- SpaceBattle.Lib.Test/RotateTest.cs
+++ SpaceBattle.Lib.Test/RotateTest.cs
@@ -10,14 +10,15 @@ namespace SpaceBattle.Lib.Test
         [Fact]
         public void TestPositiveRotate()
         {
+            //PRE
             Mock<IRotate> rotateble = new Mock<IRotate>();
-            rotateble.SetupProperty<Fraction>(r => r.Angle, new Fraction(1, 2));
-            rotateble.SetupGet<Fraction>(r => r.AngleVelocity).Returns(new Fraction(6, 3));
+            rotateble.SetupProperty<Fraction>(r => r.Angle, new Fraction(135, 3));
+            rotateble.SetupGet<Fraction>(r => r.AngleVelocity).Returns(new Fraction(180, 2));
             ICommand RC = new RotateCommand(rotateble.Object);
             //ACTION
             RC.Execute();
             //POST
-            Assert.True(Fraction.AreEquals(new Fraction(5, 2), rotateble.Object.Angle));
+            Assert.True(Fraction.AreEquals(new Fraction(135, 1), rotateble.Object.Angle));
         }
         [Fact]
         public void GetAngleExpection()
@@ -37,6 +38,14 @@ namespace SpaceBattle.Lib.Test
             rotateble.SetupGet<Fraction>(r => r.AngleVelocity).Returns(new Fraction(-5, 1));
             rotateble.SetupSet<Fraction>(r => r.Angle = It.IsAny<Fraction>()).Throws<Exception>();
             ICommand RC = new RotateCommand(rotateble.Object);
+            Assert.Throws<Exception>(() => RC.Execute());
+        }
+        [Fact]
+        public void ExpectionGetAngleVelocity()
+        {
+            Mock<IRotate> rotatable = new Mock<IRotate>();
+            rotatable.SetupGet<Fraction>(r => r.AngleVelocity).Throws<Exception>();
+            ICommand RC = new RotateCommand(rotatable.Object);
             Assert.Throws<Exception>(() => RC.Execute());
         }
     }
