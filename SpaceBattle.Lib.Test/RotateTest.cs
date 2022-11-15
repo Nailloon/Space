a0468192fa3c9de47b2d8aac7f393da9df43d7fe
Artem_Filyakin <artemfilyakin5@gmail.com>
2022-11-15 23:04:39 +0600

    Added Rotate

---
+++ SpaceBattle.Lib.Test/RotateTest.cs
@@ -0,0 +1,43 @@
+﻿using Moq;
+using SpaceBattle.Interfaces;
+using SpaceBattle.Rotate;
+using SpaceBattle.Auxiliary;
+
+namespace SpaceBattle.Lib.Test
+{
+    public class RotateTest
+    {
+        [Fact]
+        public void TestPositiveRotate()
+        {
+            Mock<IRotate> rotateble = new Mock<IRotate>();
+            rotateble.SetupProperty<Fraction>(r => r.Angle, new Fraction(1, 2));
+            rotateble.SetupGet<Fraction>(r => r.AngleVelocity).Returns(new Fraction(6, 3));
+            ICommand RC = new RotateCommand(rotateble.Object);
+            //ACTION
+            RC.Execute();
+            //POST
+            Assert.True(Fraction.AreEquals(new Fraction(5, 2), rotateble.Object.Angle));
+        }
+        [Fact]
+        public void GetAngleExpection()
+        {
+            //PRE
+            Mock<IRotate> rotateble = new Mock<IRotate>();
+            rotateble.SetupGet<Fraction>(r => r.Angle).Throws<Exception>();
+            ICommand RC = new RotateCommand(rotateble.Object);
+            //POST
+            Assert.Throws<Exception>(() => RC.Execute());
+        }
+        [Fact]
+        public void SetAngleExpection()
+        {
+            Mock<IRotate> rotateble = new Mock<IRotate>();
+            rotateble.SetupGet<Fraction>(r => r.Angle).Returns(new Fraction(10, 2));
+            rotateble.SetupGet<Fraction>(r => r.AngleVelocity).Returns(new Fraction(-5, 1));
+            rotateble.SetupSet<Fraction>(r => r.Angle = It.IsAny<Fraction>()).Throws<Exception>();
+            ICommand RC = new RotateCommand(rotateble.Object);
+            Assert.Throws<Exception>(() => RC.Execute());
+        }
+    }
+}
---
+++ SpaceBattle/Rotate/IRotate.cs
@@ -0,0 +1,10 @@
+﻿using SpaceBattle.Auxiliary;
+
+namespace SpaceBattle.Rotate
+{
+    public interface IRotate
+    {
+        Fraction Angle { get; set; }
+        Fraction AngleVelocity { get; }
+    }
+}
