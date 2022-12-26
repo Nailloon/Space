4212216f25814a0d0e3a2a6ad66859ca48719a8d
Nailloon <artemfilyakin5@gmail.com>
2022-12-26 11:19:30 +0600

    First try to do this

---
+++ SpaceBattle.Lib.Test/CheckCollisionTest.cs
@@ -0,0 +1,39 @@
+﻿using Hwdtech;
+using Moq;
+using SpaceBattle.Interfaces;
+using SpaceBattle.Collision;
+using Hwdtech.Ioc;
+
+namespace SpaceBattle.Lib.Test
+{
+    public class CheckCollisionTest
+    {
+        public CheckCollisionTest()
+        {
+            new InitScopeBasedIoCImplementationCommand().Execute();
+
+            IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+            var mockCommand = new Mock<Interfaces.ICommand>();
+            mockCommand.Setup(_command => _command.Execute());
+            var regStrategy = new Mock<IStrategy>();
+            regStrategy.Setup(_strategy => _strategy.StartStrategy(It.IsAny<object[]>())).Returns(mockCommand.Object);
+            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Calculate.Delta", (object[] args) => regStrategy.Object.StartStrategy(args)).Execute();
+            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "CollisionDecisionTree", (object[] args) => regStrategy.Object.StartStrategy(args)).Execute();
+        }
+        [Fact]
+        public void NoCollision_and_NoException()
+        {
+            var DeltaStrategy = new Mock<IStrategy>();
+            DeltaStrategy.Setup(_strategy => _strategy.StartStrategy(It.IsAny<object[]>())).Returns(new List<int>()).Verifiable();
+            var DecisionStrategy = new Mock<IStrategy>();
+            DecisionStrategy.Setup(_strategy => _strategy.StartStrategy(It.IsAny<object[]>())).Returns(false).Verifiable();
+            var UnicObject1 = new Mock<IUObject>();
+            var UnicObject2 = new Mock<IUObject>();
+            CheckCollision collision = new CheckCollision(UnicObject1.Object, UnicObject2.Object);
+            collision.Execute();
+            DecisionStrategy.Verify();
+            DeltaStrategy.Verify();
+        }
+
+    }
+}
---
+++ SpaceBattle/Collision/CheckCollision.cs
@@ -0,0 +1,26 @@
+﻿using Hwdtech;
+using SpaceBattle.Auxiliary;
+using SpaceBattle.Interfaces;
+namespace SpaceBattle.Collision
+{
+    public class CheckCollision : Interfaces.ICommand
+    {
+        IUObject UO1 { get; }
+        IUObject UO2 { get; }
+        public CheckCollision(IUObject UnicObj1, IUObject UnicObj2)
+        {
+            UO1 = UnicObj1;
+            UO2 = UnicObj2;
+        }
+
+        public void Execute()
+        {
+            Vector vec = IoC.Resolve<Vector>("Calculate.Delta", UO1, UO2);
+            bool collision = IoC.Resolve<bool>("CollisionDecisionTree", vec);
+            if (collision)
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
