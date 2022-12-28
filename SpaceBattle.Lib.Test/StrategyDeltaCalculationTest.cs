27b3137db7809e9a09c0eda1bdf2d5b37dab6c76
Nailloon <artemfilyakin5@gmail.com>
2022-12-28 21:18:52 +0600

    good tests for CheckCollision

---
+++ SpaceBattle.Lib.Test/StrategyDeltaCalculationTest.cs
@@ -0,0 +1,19 @@
+﻿using Moq;
+using SpaceBattle.Collision;
+using SpaceBattle.Interfaces;
+
+namespace SpaceBattle.Lib.Test
+{
+    public class StrategyDeltaCalculationTest
+    {
+        [Fact]
+        public void DeltaCalculationTest()
+        {
+            var UnicObject1 = new Mock<IUObject>();
+            var UnicObject2 = new Mock<IUObject>();
+            var DeltaStrategy = new StrategyDeltaCalculation();
+            DeltaStrategy.StartStrategy(UnicObject1, UnicObject2);
+        }
+
+    }
+}
--- SpaceBattle/Collision/StrategyDeltaCalculation.cs
+++ SpaceBattle/Collision/StrategyDeltaCalculation.cs
@@ -12,11 +12,10 @@ namespace SpaceBattle.Collision
             var uObj2 = (IUObject)args[1];
             List<string> names = new List<string> { "Coords", "Velocity" };
             List<int> useful = new List<int>();
-            //foreach ()string name in names
-            for (int i = 0; i < 2; i++)
+            foreach (string name in names)
             {
-                var uObj1Vec = IoC.Resolve<Vector>("UObject.GetProperty", uObj1, names[i]);
-                var uObj2Vec = IoC.Resolve<Vector>("UObject.GetProperty", uObj2, names[i]);
+                var uObj1Vec = IoC.Resolve<Vector>("UObject.GetProperty", uObj1, name);
+                var uObj2Vec = IoC.Resolve<Vector>("UObject.GetProperty", uObj2, name);
                 useful.Add(uObj1Vec[0] - uObj2Vec[0]);
                 useful.Add(uObj1Vec[1] - uObj2Vec[1]);
             }
