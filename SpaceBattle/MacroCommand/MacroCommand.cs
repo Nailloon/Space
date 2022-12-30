035b7be608dff729af476584da99eda19c8d4f08
Nailloon <artemfilyakin5@gmail.com>
2022-12-30 09:20:11 +0600

    MacroCommand Class

---
+++ SpaceBattle/MacroCommand/MacroCommand.cs
@@ -0,0 +1,20 @@
+﻿using SpaceBattle.Interfaces;
+namespace SpaceBattle.MacroCommand
+
+{
+    public class MacroCommand : Interfaces.ICommand
+    {
+        IEnumerable<Interfaces.ICommand> commands;
+        public MacroCommand(IEnumerable<Interfaces.ICommand> commands)
+        {
+            this.commands = commands;
+        }
+        public void Execute()
+        {
+            foreach (Interfaces.ICommand command in commands)
+            {
+                command.Execute();
+            }
+        }
+    }
+}
