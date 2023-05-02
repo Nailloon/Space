e1501700bb440513927ff43ef67d8666d24c4068
Nailloon <artemfilyakin5@gmail.com>
2023-05-02 07:52:09 +0600

    Убрана частичная зависимость от реализации, распланированы тесты

--- SpaceBattle.Lib.Test/MyThreadTest.cs
+++ SpaceBattle.Lib.Test/MyThreadTest.cs
@@ -18,7 +18,31 @@ namespace SpaceBattle.Lib.Test
             IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
             var threadDict = new ConcurrentDictionary<string, MyThread>();
             var senderDict = new ConcurrentDictionary<string, SenderAdapter>();
+            IoC.Resolve<ICommand>("IoC.Register", "ThreadIDMyThreadMapping", () => threadDict).Execute();
+            IoC.Resolve<ICommand>("IoC.Register", "ThreadIDSenderMapping", () => senderDict).Execute();
+            IoC.Resolve<ICommand>("IoC.Register", "SenderAdapterGetByID", (string id) => senderDict[id]).Execute();
+            IoC.Resolve<ICommand>("IoC.Register", "ServerThreadGetByID", (string id) => threadDict[id]).Execute();
         }
+        [Fact]
+        public void MyThreadHardStopTest()
+        {
 
+        }
+        public void MyThreadSoftStopTest()
+        {
+
+        }
+        public void MyThreadCreateTest()
+        {
+
+        }
+        public void MyThreadUpdateBehaviorTest()
+        {
+
+        }
+        public void MyThreadWorkingTogetherTest()
+        {
+
+        }
     }
 }
--- SpaceBattle/ServerStrategies/SoftStopStrategy.cs
+++ SpaceBattle/ServerStrategies/SoftStopStrategy.cs
@@ -12,7 +12,7 @@ namespace SpaceBattle.ServerStrategies
             var id = args[0];
             Action? act = (Action?)args[1];
             var MT = IoC.Resolve<MyThread>("ServerThreadGetByID", id);
-            var sender = IoC.Resolve<SenderAdapter>("SenderAdapterGetByID", id);
+            var sender = IoC.Resolve<ISender>("SenderAdapterGetByID", id);
             var hardStopCommand = new ThreadStopCommand(MT, act);
             var softStopCommand = IoC.Resolve<ICommand>("SendCommand", sender, hardStopCommand);
             return IoC.Resolve<ICommand>("SendCommand", sender, softStopCommand);
