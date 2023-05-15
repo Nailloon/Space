146b08a3add66299d5c3e616abbc3f3547f9c62e
Nailloon <artemfilyakin5@gmail.com>
2023-05-15 23:54:20 +0600

    Commands

---
+++ SpaceBattle/Server/SendCommand.cs
@@ -0,0 +1,19 @@
+﻿using SpaceBattle.Interfaces;
+using ICommand = SpaceBattle.Interfaces.ICommand;
+namespace SpaceBattle.Server
+{
+    public class SendCommand : ICommand
+    {
+        private ISender sndr;
+        private ICommand cmd;
+        public SendCommand(ISender sndr, ICommand cmd)
+        {
+            this.sndr = sndr;
+            this.cmd = cmd;
+        }
+        public void Execute()
+        {
+            sndr.Send(cmd);
+        }
+    }
+}
---
+++ SpaceBattle/Server/ThreadStopCommand.cs
@@ -0,0 +1,24 @@
+﻿using ICommand = SpaceBattle.Interfaces.ICommand;
+
+namespace SpaceBattle.Server
+{
+    public class ThreadStopCommand: ICommand
+    {
+        MyThread stoppingThread;
+    public ThreadStopCommand(MyThread stoppingThread)
+    {
+        this.stoppingThread = stoppingThread;
+    }
+    public void Execute()
+    {
+        if (stoppingThread.Equals(Thread.CurrentThread))
+        {
+            stoppingThread.Stop();
+        }
+        else
+        {
+            throw new Exception();
+        }
+    }
+}
+}
