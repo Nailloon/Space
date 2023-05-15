1e066585fe564a3e29037ef23e7a6b9b467f5cfc
Nailloon <artemfilyakin5@gmail.com>
2023-05-15 23:49:15 +0600

    Base of thread

---
+++ SpaceBattle/Interfaces/IReceiver.cs
@@ -0,0 +1,8 @@
+﻿namespace SpaceBattle.Interfaces
+{
+    public interface IReceiver
+    {
+        public ICommand Receive();
+        public bool IsEmpty();
+    }
+}
---
+++ SpaceBattle/Interfaces/ISender.cs
@@ -0,0 +1,7 @@
+﻿namespace SpaceBattle.Interfaces
+{
+    public interface ISender
+    {
+        public void Send(ICommand command);
+    }
+}
