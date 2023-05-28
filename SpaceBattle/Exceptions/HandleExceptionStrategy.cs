ee7196982967d5bfdd856748392ff2e701eaaf70
Nailloon <artemfilyakin5@gmail.com>
2023-05-28 12:18:22 +0600

    The base of GameCommand

---
+++ SpaceBattle/Exceptions/HandleExceptionStrategy.cs
@@ -0,0 +1,26 @@
+﻿using ICommand = SpaceBattle.Interfaces.ICommand;
+using SpaceBattle.Interfaces;
+using Hwdtech;
+
+namespace SpaceBattle.Exceptions
+{
+    public class HandleExceptionStrategy : IStrategy
+    {
+        public object StartStrategy(params object[] args)
+        {
+            var exception = (Exception)args[0];
+            var command = (ICommand)args[1];
+
+            var dictExceptionHandlers = IoC.Resolve<IDictionary<Exception, IDictionary<ICommand, IStrategy>>>("Dictionary.Handler.Exception");
+
+            if (!dictExceptionHandlers.ContainsKey(exception) || !dictExceptionHandlers[exception].ContainsKey(command))
+            {
+                throw new KeyNotFoundException("Handler not found for the command and exception");
+            }
+            else
+            {
+                return dictExceptionHandlers[exception][command];
+            }
+        }
+    }
+}
---
+++ SpaceBattle/GameCommand/RepeatGameCommand.cs
@@ -0,0 +1,29 @@
+﻿using ICommand = SpaceBattle.Interfaces.ICommand;
+using Hwdtech;
+
+namespace SpaceBattle.GameCommand
+{
+    public class RepeatGameCommand: ICommand
+    {
+        string id;
+        GameCommand gameCommand;
+        object scope;
+        public RepeatGameCommand(string id, object scope)
+        {
+            this.id = id;
+            gameCommand = new GameCommand(id);
+            this.scope = scope;
+        }
+        public void Execute()
+        {
+            var initialScope = IoC.Resolve<object>("Scopes.Current");
+            IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", this.scope).Execute();
+
+            this.gameCommand.Execute();
+            var threadID = IoC.Resolve<string>("Storage.GetThreadByGameID", this.id);
+            IoC.Resolve<ICommand>("SendCommandByThreadIDStrategy", threadID, this).Execute();
+
+            IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", initialScope).Execute();
+        }
+    }
+}
