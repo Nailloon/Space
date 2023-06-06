9402613eb18508ebd0d5953b4a92593902e4d5a9
Nailloon <artemfilyakin5@gmail.com>
2023-06-06 11:11:42 +0600

    IBuilder and reflection. Need to rewrite template and other

---
+++ SpaceBattle/AdapterGenerating/AdapterBuilderStrategy.cs
@@ -0,0 +1,22 @@
+﻿using Hwdtech;
+using SpaceBattle.Interfaces;
+using System.Reflection;
+
+namespace SpaceBattle.AdapterGenerating
+{
+    public class AdapterGeneratorStrategy: IStrategy
+    {
+        public object StartStrategy(params object[] args)
+        {
+            var dtype = (Type)args[0];
+            var dtype1 = (Type)args[1];
+            var builder = IoC.Resolve<IBuilder>("IninitialAdapterCode", "Dtype", dtype, dtype1);
+            var properties = dtype.GetProperties();
+            foreach (PropertyInfo property in properties)
+            {
+                builder.addMembers("Property", property);
+            }
+            return builder.Build();
+        }
+    }
+}
---
+++ SpaceBattle/AdapterGenerating/AdapterGenerator.cs
@@ -0,0 +1,69 @@
+﻿using Scriban;
+using SpaceBattle.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceBattle.AdapterGenerating
+{
+    public class AdapterGenerator: IBuilder
+    {
+        private Template template = Template.Parse(text:
+        @"
+namespace SpaceBattle;
+public class {{ class_name }} : {{ int_name }} {
+    private System.Collections.Generic.IDictionary<string, object> data;
+    public {{ class_name }}(System.Collections.Generic.IDictionary<string, object> target) {
+        this.target = terget;
+    }
+    {{- for property in properties }}
+    {{ full_property_name = property.type + ""."" + property.name }}
+    public {{ property.type }} {{ property.name }} {
+        {{
+            if property.can_read
+                ""get => ("" + property.type + "")data[\"""" + full_property_name + ""\""];""
+            end
+            if property.can_write
+                ""set => IoC.Resolve<ICommand>("" + UObject.setValue + "", object, propertyName, propertyValue).Execute()""
+            end
+        }}
+    }
+    {{- end }}
+        ");
+        private IEnumerable<PropertyInfo> propertyInfos;
+        private Type dtype;
+        private readonly IDictionary<string, Action<object[]>> configHandlers = new Dictionary<string, Action<object[]>>{
+        {"Property",
+            args => {
+                var ctx = (AdapterGenerator)args[0];
+                var propInfo = (PropertyInfo)args[1];
+                ctx.propertyInfos = ctx.propertyInfos.Append(propInfo).ToArray();
+            }
+        },
+        {"Dtype",
+            args => {
+                var ctx = (AdapterGenerator)args[0];
+                ctx.dtype = (Type)args[1];
+            }
+        }
+        };
+        public AdapterGenerator()
+        {
+            this.propertyInfos = new LinkedList<PropertyInfo>();
+            this.dtype = null!;
+        }
+        public IBuilder addMembers(string param, params object[] args)
+        {
+            Action<object[]> handler = configHandlers[param];
+            handler(new object[] { this }.Concat(args).ToArray());
+            return this;
+        }
+        public object Build()
+        {
+            return;
+        }
+    }
+}
