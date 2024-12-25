b9a11d4b6cfec16eea7433f0767c242adb648a12
Nailloon <artemfilyakin5@gmail.com>
2024-12-25 07:28:12 +0600

    tests: fix old test

--- SpaceBattle.Lib.Test/EndpointTest.cs
+++ SpaceBattle.Lib.Test/EndpointTest.cs
@@ -1,5 +1,4 @@
 using Hwdtech;
-using Hwdtech.Ioc;
 using Moq;
 using System.Collections.Concurrent;
 using SpaceBattle.Interfaces;
@@ -10,6 +9,7 @@ using SpaceBattle.ServerStrategies;
 using SpaceBattle.gRPC.Services;
 using SpaceBattle.gRPC.Router;
 using SpaceBattle.gRPC.Others;
+using SpaceBattle.gRPC;
 
 namespace SpaceBattle.Lib.Test;
 
@@ -19,52 +19,80 @@ public class EndpointTest
     public void PositiveRoutingTest()
     {
         new Hwdtech.Ioc.InitScopeBasedIoCImplementationCommand().Execute();
-    Hwdtech.IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", Hwdtech.IoC.Resolve<object>("Scopes.New", Hwdtech.IoC.Resolve<object>("Scopes.Root"))).Execute();
+        Hwdtech.IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", Hwdtech.IoC.Resolve<object>("Scopes.New", Hwdtech.IoC.Resolve<object>("Scopes.Root"))).Execute();
 
-    var threadDict = new ConcurrentDictionary<string, MyThread>();
-    var gamesThreadsDictionary = new ConcurrentDictionary<string, string>();
-    var senderDict = new ConcurrentDictionary<string, ISender>();
-    var orderSenderDict = new ConcurrentDictionary<string, ISender>();
-    var gamesDictionary = new Dictionary<string, Queue<ICommand>>();
-    Queue<ICommand> gameQueue1 = new Queue<ICommand>();
-    gamesDictionary.TryAdd("1", gameQueue1);
-    ICommand gameCommand1 = new GameCommand("1", gameQueue1);
-    gamesThreadsDictionary.TryAdd("1", "80");
+        var threadDict = new ConcurrentDictionary<string, MyThread>();
+        var gamesThreadsDictionary = new ConcurrentDictionary<string, string>();
+        var senderDict = new ConcurrentDictionary<string, ISender>();
+        var senderOrderDict = new ConcurrentDictionary<string, ISender>();
+        var gamesDictionary = new Dictionary<string, Queue<ICommand>>();
+        Queue<ICommand> gameQueue1 = new Queue<ICommand>();
+        gamesDictionary.TryAdd("1", gameQueue1);
+        Queue<ICommand> gameQueue2 = new Queue<ICommand>();
+        gamesDictionary.TryAdd("2", gameQueue2);
+        ICommand gameCommand1 = new GameCommand("1", gameQueue1);
+        ICommand gameCommand2 = new GameCommand("2", gameQueue2);
+        gamesThreadsDictionary.TryAdd("1", "80");
+        gamesThreadsDictionary.TryAdd("2", "80");
 
-    Hwdtech.IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "ThreadIDMyThreadMapping", (object[] _) => threadDict).Execute();
-    Hwdtech.IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "ThreadIDSenderMapping", (object[] _) => senderDict).Execute();
-    Hwdtech.IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "ThreadIDOrdersSenderMapping", (object[] _) => orderSenderDict).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "ThreadIDMyThreadMapping", (object[] _) => threadDict).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "ThreadIDSenderMapping", (object[] _) => senderDict).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "ThreadIDOrdersSenderMapping", (object[] _) => senderOrderDict).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "ServerThreadGetByID", (object[] id) => threadDict[(string)id[0]]).Execute();
 
-    var createAndStartThreadStrategy = new CreateAndStartThreadStrategy();
-    Hwdtech.IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "CreateAndStartThread", (object[] args) => createAndStartThreadStrategy.StartStrategy(args)).Execute();
-    Hwdtech.IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "QuantumForGame", (object[] _) => (object)new TimeSpan(0, 0, 0, 40, 0)).Execute();
-    var protobufMapToDictionaryStrategy = new ProtobufMapToDictionaryStrategy();
-    Hwdtech.IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "ProtobufMapToDictionary", (object[] args) => protobufMapToDictionaryStrategy.StartStrategy(args)).Execute();
-    Hwdtech.IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "SendCommandToGame", (object[] args) => gamesDictionary[(string)args[0]].Append((ICommand)args[1])).Execute();
+        var createWithStartThreadStrategy = new CreateAndStartThreadStrategy();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "CreateWithStartThread", (object[] args) => createWithStartThreadStrategy.StartStrategy(args)).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "QuantumForGame", (object[] _) => (object)new TimeSpan(0, 0, 0, 0, 150)).Execute();
+        var command1 = new Mock<ICommand>();
+            var regStrategy1 = new Mock<IStrategy>();
+            command1.Setup(_command => _command.Execute());
+            regStrategy1.Setup(_strategy => _strategy.StartStrategy(It.IsAny<object[]>())).Returns(command1.Object);
 
-    BlockingCollection<ICommand> queue = new();
-    BlockingCollection<ICommand> orderQueue = new();
-    ISender orderSender = new SenderAdapter(orderQueue);
-    IReceiver orderReceiver = new ReceiverAdapter(orderQueue);
-    ISender sender = new SenderAdapter(queue);
-    IReceiver receiver = new ReceiverAdapter(queue);
-    var th1 = Hwdtech.IoC.Resolve<MyThread>("CreateAndStartThread", "80", sender, receiver, orderSender, orderReceiver);
+        BlockingCollection<ICommand> queue = new();
+        BlockingCollection<ICommand> orderQueue = new();
+        ISender orderSender = new SenderAdapter(orderQueue);
+        IReceiver orderReceiver = new ReceiverAdapter(orderQueue);
+        ISender sender = new SenderAdapter(queue);
+        IReceiver receiver = new ReceiverAdapter(queue);
 
-    var valueMap = new Google.Protobuf.Collections.MapField<string, string>(){{"type", "Move"},{"objid", "uobj1"},{"velocity", "5"}};
-    ICommand commandForGame = new ActionCommand(()=>{
-        var uobj = IoC.Resolve<IUObject>("GetUObject", "uobj1");
-        uobj.set_property("MoveVelocity", 5);
-    });
+        var mre0 = new ManualResetEvent(false);
+        var mre1 = new ManualResetEvent(false);
 
-    Hwdtech.IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "OrderDictionaryToICommand", (object[] args) => commandForGame).Execute();
+        var valueMap = new Google.Protobuf.Collections.MapField<string, string>(){{"type", "Move"},{"objid", "uobj1"},{"velocity", "5"}};
+        OrderRequest orderRequest = new()
+        {
+            GameId = "2"
+        };
+        orderRequest.Map.Add(valueMap);
 
-    IRouter router = new Router(gamesThreadsDictionary, orderSenderDict);
-    EndPointService endpoint = new EndPointService(router);
+        var MockCommand = new Mock<ICommand>();
+        MockCommand.Setup(x => x.Execute());
+                            ICommand commandForGame = new ActionCommand(()=>{
+                        MockCommand.Object.Execute();
+        });
+        IRouter router = new Router(gamesThreadsDictionary, senderOrderDict);
+        Action act1 = () => {
+                IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+                IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "OrderDictionaryToICommand", (object[] args) => commandForGame).Execute();
+                IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "SendCommandToGame", (object[] args) => new ActionCommand(()=>gameQueue2.Append((ICommand)args[1]))).Execute();
+                IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "HandleException", (object[] args) => regStrategy1.Object.StartStrategy(args)).Execute();
+                mre0.Set();
+            };
+        var ST = new MyThread(receiver, orderReceiver);
+        senderDict.TryAdd("80", sender);
+        threadDict.TryAdd("80", ST);
+        senderOrderDict.TryAdd("80", orderSender);
+        sender.Send(new ActionCommand(act1));
+        ST.Execute();
 
-        Assert.Empty(gameQueue1);
-        router.route("1", valueMap);
-        Assert.NotEmpty(orderQueue);
+        mre0.WaitOne();
+        router.route(orderRequest.GameId, orderRequest.Map);
+        sender.Send(gameCommand1);
+        sender.Send(gameCommand2);
+        sender.Send(new ActionCommand(()=>{mre1.Set();}));
+        mre1.WaitOne();
+        Assert.Empty(orderQueue);
+        Assert.Empty(gameQueue2);
+        MockCommand.Verify();
     }
-
-
 }
